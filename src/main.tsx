116c72308a9914e185d64c96290279477eb882c1
Oleg Polyakov <olegpolyakov@outlook.com>
2026-01-16 15:00:24 +0300

    fix: update kantanui styles import to use pkg syntax

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'kantanui';
 
 import App from './App';
 
-import 'kantanui/dist/index.css';
+import 'kantanui/styles';
 import './styles.scss';
 
 createRoot(document.getElementById('root')!).render(
