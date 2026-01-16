3f8fb6cb460966f2675183b8f73d4e9754a8ae09
Oleg Polyakov <olegpolyakov@outlook.com>
2026-01-16 14:48:48 +0300

    refactor: update vite.config.js to support environment-based configuration

--- vite.config.js
+++ vite.config.js
@@ -6,41 +6,46 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import mdx from '@mdx-js/rollup';
 
-export default defineConfig({
-    root: path.resolve(__dirname, 'src'),
-    publicDir: path.resolve(__dirname, 'public'),
-    build: {
-        outDir: path.resolve(__dirname, 'dist'),
-        emptyOutDir: true,
-        sourcemap: true
-    },
-    plugins: [
-        react(),
-        mdx(),
-        svgr({
-            include: '**/*.svg',
-            svgrOptions: {
-                ref: true,
-                svgo: true,
-                titleProp: true
-            }
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            'kantanui': path.resolve(__dirname, '../../kantanui')
-        }
-    },
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly',
-            generateScopedName: '[folder]__[local]--[hash:base64:5]'
+export default defineConfig(env => {
+    const isDevelopment = env.mode === 'development';
+
+    return {
+        root: path.resolve(__dirname, 'src'),
+        publicDir: path.resolve(__dirname, 'public'),
+        build: {
+            outDir: path.resolve(__dirname, 'dist'),
+            emptyOutDir: true,
+            sourcemap: true
         },
-        preprocessorOptions: {
-            scss: {
-                importers: [new NodePackageImporter()]
+        plugins: [
+            react(),
+            mdx(),
+            svgr({
+                include: '**/*.svg',
+                svgrOptions: {
+                    ref: true,
+                    svgo: true,
+                    titleProp: true
+                }
+            })
+        ],
+        resolve: {
+            alias: Object.assign({
+                '@': path.resolve(__dirname, 'src')
+            }, isDevelopment ? {
+                'kantanui': path.resolve(__dirname, '../../kantanui')
+            } : {})
+        },
+        css: {
+            modules: {
+                localsConvention: 'camelCaseOnly',
+                generateScopedName: '[folder]__[local]--[hash:base64:5]'
+            },
+            preprocessorOptions: {
+                scss: {
+                    importers: [new NodePackageImporter()]
+                }
             }
         }
-    }
+    };
 });
